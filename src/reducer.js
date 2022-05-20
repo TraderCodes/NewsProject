f62f4966acd2e3619ce968abb30df646cbcfed23
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-05-20 04:23:28 -0700

    ✔ Setting reducer + loading function

--- src/reducer.js
+++ src/reducer.js
@@ -1,10 +1,18 @@
 import {
-  SET_LOADING,
-  SET_STORIES,
-  REMOVE_STORY,
-  HANDLE_PAGE,
-  HANDLE_SEARCH,
-} from './actions'
+   SET_LOADING,
+   SET_STORIES,
+   REMOVE_STORY,
+   HANDLE_PAGE,
+   HANDLE_SEARCH,
+} from './actions';
 
-const reducer = () => {}
-export default reducer
+const reducer = (state, action) => {
+   switch (action.type) {
+      case SET_LOADING:
+         // return previous value using state
+         return { ...state, isLoading: true };
+      default:
+         throw new Error(`${action.type}`);
+   }
+};
+export default reducer;
